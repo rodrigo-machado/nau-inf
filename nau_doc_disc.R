40bba77dc4e532b3105d551c8fa187624cb7956e
Marcus <allbits@github.com>
2016-06-09 11:48:29 -0300

    Introduced weights.

--- nau_doc_disc.R
+++ nau_doc_disc.R
@@ -13,9 +13,14 @@ wrap.it <- function(x, len) {
   sapply(x, function(y) paste(strwrap(y, len), collapse = "\n"), USE.NAMES = FALSE)
 }
 
+## weights
+w=c(0.08,0.1,0.07,0.1,0.05,0.1,0.08,0.08,0.13,0.05,0.16)
+qcol=c("q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11")
+
 ## evaluate the courses of a given semester
 ## input: a data file `f`, the name of the year, the format
 evaluateSemester = function(f,year,format="v1") {
+    ## (0) read data, apply weights
     d=read.csv2(f)
     switch(format) 
     colnames(d)=switch(format,
@@ -23,7 +28,14 @@ evaluateSemester = function(f,year,format="v1") {
                 v2=c("unid","dept","curso","disc","turma","q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11","avg","grp1","grp2","grp3","grp4","grp5","tot"), ## 2015-2, mine
                 v3=c("unid","dept","curso","disc","turma","q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11") ## 2015-2
                        )
-
+    ## (0.1) apply the weights
+    d$tw=apply(d[,qcol],1,function(x) { weighted.mean(!is.na(x),w,na.rm=T) })
+    for (i in 1:length(qcol)) {
+        d[,qcol[i]]=10*w[i]*d[,qcol[i]]/d$tw
+    }
+    ## (0.2) for verification: compute the means
+    d$nm=apply(d[,qcol],1,function(x) { ifelse(sum(is.na(x))==length(qcol),NA,sum(x,na.rm=T)) })
+    
     ## (1) clean up data: remove all programs with less than 10 responses
     fewR=subset(ddply(d,.(curso),summarize,N=length(turma)),N<10)$curso
     e=subset(d,!(curso %in% fewR))
@@ -35,11 +47,9 @@ evaluateSemester = function(f,year,format="v1") {
     f=subset(f,N>4) ## remove all courses with 4 evaluations or less
 
     ## (3) produce a overview plot
-    ##pdf(paste0("overall-",year,".pdf"),11,7)
     op <- par(mar = c(5,14,4,2) + 0.1)
     with(f,barplot(rbind(q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11),names=paste(disc," (",N,")",sep=""),horiz=T,las=1,cex.names=0.5,col=brewer.pal(11,"Oranges"),border=NA,xlab="Average number of points (number of samples in parentheses)",main=paste0("Evaluação geral disciplinas em ",year,"\n(somente disciplinas com 5 ou mais avaliações)"),legend.text=c("1","2","3","4","5","6","7","8","9","10","11"),args.legend = list(x = "bottomright", title="Questão", ncol = 2)))
     grid()
-    ##dev.off()
     par(op)
     list(d,e,f,year)
 }
@@ -61,10 +71,8 @@ compareSemesters = function(s1,s2) {
     ranks$label=wrap.it(ranks$disc,20)
     
     ## (3) create a plot
-    ##pdf("comp-15-averages.pdf",10,5)
     ggplot(data=ranks)+geom_abline(intercept=0,slope=1,color="white")+geom_abline(intercept=-0.5,slope=1,color="white")+geom_abline(intercept=0.5,slope=1,color="white")+geom_segment(data=ranks,aes(x=x,y=y,xend=m.x,yend=m.y),color="gray")+geom_point(aes(x=m.x,y=m.y),color="red")+labs(x=paste0("Média em ",s1[[4]]),y=paste0("Média em ",s2[[4]]),title=paste0("Comparação dos semestres ",s1[[4]]," e ",s2[[4]]))+geom_text(aes(x=x,y=y,label=label),size=2,hjust=0)
     ## +xlim(c(2,5))+ylim(c(2,5))
-    ##dev.off()
 }
 
 ## compare classes for semester `s`
@@ -72,7 +80,7 @@ compareSemesters = function(s1,s2) {
 compareClasses = function(s) {
     ## (1) aggregate data, count number of classes
     bycd=ddply(s[[2]],.(disc,turma),summarize,N=length(q1),q1=mean(q1,na.rm=T),q2=mean(q2,na.rm=T),q3=mean(q3,na.rm=T),q4=mean(q4,na.rm=T),q5=mean(q5,na.rm=T),q6=mean(q6,na.rm=T),q7=mean(q7,na.rm=T),q8=mean(q8,na.rm=T),q9=mean(q9,na.rm=T),q10=mean(q10,na.rm=T),q11=mean(q11,na.rm=T))
-    bycd$m=rowMeans(bycd[,c("q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11")],na.rm=T)
+    bycd$m=rowMeans(bycd[,qcol],na.rm=T)
     bycd=subset(bycd,N>4)
     bycd=merge(bycd,ddply(bycd,.(disc),summarize,Nt=length(N)),by="disc")
     bycd.2=subset(bycd,Nt==2)
@@ -88,9 +96,7 @@ compareClasses = function(s) {
     bycd.2a$label=wrap.it(bycd.2a$disc,20)
     
     ## (3) plot it
-    ##pdf(paste0("correlation-",year,".pdf"),11,7)
     print(ggplot(data=bycd.2a)+geom_abline(intercept=0,slope=1,color="white")+geom_abline(intercept=-0.5,slope=1,color="white")+geom_abline(intercept=0.5,slope=1,color="white")+geom_segment(aes(x=x,y=y,xend=m.x,yend=m.y),color="gray")+geom_point(aes(x=m.x,y=m.y),color="red")+labs(x="Nota média turma A",y="Nota média turma B",title=paste0("Comparação das disciplinas em ",s[[4]]," com duas turmas\n(somente turmas com mais que 4 respostas)"))+geom_text(aes(x=x,y=y,label=label),size=2,hjust=0))
-    ##dev.off()
     list(bycd.3,s[[4]])
 }
 
@@ -110,9 +116,9 @@ questions=c(
 evaluateQuestions = function(s) {
     ## (1) melt it, define a couple of auxiliary factors, cut it
     ms=subset(melt(s[[1]]),grepl("q",variable))
-    ms$variable=factor(ms$variable,ordered=T,levels=c("q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11"))
-    ms$variable20=factor(ms$variable,ordered=T,levels=rev(c("q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11")),labels=rev(wrap.it(questions,20)))
-    ms$variable55=factor(ms$variable,ordered=T,levels=rev(c("q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11")),labels=rev(wrap.it(questions,55)))
+    ms$variable=factor(ms$variable,ordered=T,levels=qcol)
+    ms$variable20=factor(ms$variable,ordered=T,levels=rev(qcol),labels=rev(wrap.it(questions,20)))
+    ms$variable55=factor(ms$variable,ordered=T,levels=rev(qcol),labels=rev(wrap.it(questions,55)))
     ms$dvalue=cut(ms$value,breaks=c(1:5),include.lowest=T)
 
     ## (2) plot it
@@ -187,7 +193,7 @@ dev.off()
 #################### leftovers
 
 ## validation of means
-rowMeans(y15s1[[1]][,c("q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11")],na.rm=T)
+rowMeans(y15s1[[1]][,qcol],na.rm=T)
 
 ## number of student feedback per course
 fb=ddply(y15s1[[1]],.(curso),summarize,N=length(turma))
